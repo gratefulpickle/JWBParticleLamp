9a1a91e3ea63663c82d549df5b532b2ae1755786
JWB <jeremyfws@gmail.com>
2018-02-07 16:18:47 -0500

    add non-async methods

--- ParticleLamp.cs
+++ ParticleLamp.cs
@@ -12,56 +12,65 @@ namespace JWBParticleLamp
         public string DeviceID { get; set; }
         public string Token { get; set; }
         public string LastResponse { get; set; }
+        public bool Async { get; set; }
 
         public ParticleLamp() { }
 
 
-        public async void Trigger(int eventid = 0)
+        public void Trigger(int eventid = 0)
         {
-            await RemoteInvoke("Trigger", eventid.ToString());
-
+            RemoteInvoke("Trigger", eventid.ToString());
         }
 
-        public async Task RemoteInvoke(string cmd, string args = "")
+        public async void TriggerAsync(int eventid = 0)
         {
-            await Task.Run(() =>
+            await RemoteInvokeAsync("Trigger", eventid.ToString());
+        }
+
+        public async Task RemoteInvokeAsync(string cmd, string args = "")
+        {
+            await Task.Run((Action)(() =>
             {
-                var url = GetDeviceURL() + "/" + cmd;
-
-                var postData = "args=" + args.ToString();
-                var data = Encoding.ASCII.GetBytes(postData);
-                try
-                {
-
-                    var request = (HttpWebRequest)WebRequest.Create(url);
-
-                    request.Method = "POST";
-
-                    request.ContentType = "application/x-www-form-urlencoded";
-                    request.Accept = "application/json";
-                    request.Headers.Add("Authorization", "Bearer " + Token);
-                    request.ContentLength = data.Length;
-                    request.UserAgent = "Mozilla/4.0 (Windows 7 6.1) Java/1.6.0_26";
-                    request.Proxy = null;
-
-                    using (var stream = request.GetRequestStream())
-                    {
-                        stream.Write(data, 0, data.Length);
-                    }
-
-
-                    var response = request.GetResponse() as HttpWebResponse;
-
-                    LastResponse = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                }
-                catch (Exception Ex)
-                {
-                    LastResponse = Ex.Message;
-                }
-            });
+                this.RemoteInvoke((string)cmd, (string)args);
+            }));
 
         }
 
+        private void RemoteInvoke(string cmd, string args)
+        {
+            var url = GetDeviceURL() + "/" + cmd;
+
+            var postData = "args=" + args.ToString();
+            var data = Encoding.ASCII.GetBytes(postData);
+            try
+            {
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+
+                request.Method = "POST";
+
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "application/json";
+                request.Headers.Add("Authorization", "Bearer " + Token);
+                request.ContentLength = data.Length;
+                request.UserAgent = "Mozilla/4.0 (Windows 7 6.1) Java/1.6.0_26";
+                request.Proxy = null;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+
+                var response = request.GetResponse() as HttpWebResponse;
+
+                LastResponse = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
+            catch (Exception Ex)
+            {
+                LastResponse = Ex.Message;
+            }
+        }
 
         private string GetDeviceURL()
         {
